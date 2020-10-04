813655eb8919d8f65fbe3552cf476b86b152e834
siyual-park <siyual.bak@gmail.com>
2020-10-04 17:34:46 +0900

    리팩토링

--- src/test/kotlin/com/ara/converter/mock/User.kt
+++ src/test/kotlin/com/ara/converter/mock/User.kt
@@ -1,11 +1,7 @@
 package com.ara.converter.mock
 
-import java.time.LocalDate
-
 data class User(
     val username: String,
     val password: String,
     val id: String? = null
-) {
-    val createdAt: LocalDate = LocalDate.now()
-}
+)
